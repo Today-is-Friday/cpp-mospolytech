2b4c859bbc112118a7b9ede2b4c484f791ba3368
Today-is-Friday <egorka.ivanov25@gmail.com>
2025-11-16 18:04:35 +0300

    lab5-2 minor adjustment

--- lab5/magicional_gun.cpp
+++ lab5/magicional_gun.cpp
@@ -2,7 +2,7 @@
 
 #include "lab5.h"
 
-MagicianGun::MagicianGun() : additional_dmg(0.0) {}
+MagicianGun::MagicianGun() : Gun(), additional_dmg(0.0) {}
 
 MagicianGun::MagicianGun(std::string name_, GunType gun_type_, double dmg_,
                          double weight_, double additional_dmg_)
